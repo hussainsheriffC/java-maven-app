81d12159b55fa1b387fa4a228f2d20707f337622
hussainsheriffC <hussain.sheriff@cloudreach.com>
2023-03-16 16:43:23 +0530

    updated file

--- script.groovy
+++ script.groovy
@@ -1,25 +1,35 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+//for the first Jenkisfile
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
-
-return this
-
-
-// def function(){
+// def buildApp(){
 //     echo 'building the application...'
 // }
+
+// def testApp(){
+//     echo 'testing the application...'
+// }
+
+// def deployApp(){
+//     echo 'deplying the application...'
+//     echo "deploying version ${params.VERSION}"
+// }
 // return this
+
+//for the second Jenkins file
+def buildJar(){
+    echo 'building the application...'
+    sh 'mvn package'
+}
+
+def buildImage(){
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t hussainsheriffj/demo-app:jma-2.1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push hussainsheriffj/demo-app:jma-2.1'
+    }
+}
+
+def deployApp(){
+    echo 'deplying the application...'
+}
+return this
