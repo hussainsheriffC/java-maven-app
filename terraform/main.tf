ea7479afe30c1f7a9d98283098bd155c08b98456
hussainsheriffC <hussain.sheriff@cloudreach.com>
2023-04-28 01:14:42 +0530

    ssh agent 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "myapp-bucket2324234hh"
+    key = "myapp-bucket2324234hh/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
